9141dd1746c91cd70aa6d5c1853b860acba04947
cyril314 <svip.cyril@gmail.com>
2026-06-16 19:02:46 +0800

    update code

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 const nextConfig: NextConfig = {
   /* config options here */
   // Cloudflare环境配置
   serverExternalPackages: [],
-
 };
+
+initOpenNextCloudflareForDev();
 
 export default nextConfig;
